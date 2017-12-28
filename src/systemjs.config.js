0c14a65e7fe267366409ce130bbb779df4eeeb59
sureshkumar <sureshkumar.palanisamy@syncfusion.com>
2017-12-28 17:59:05 +0530

    model samples committed

--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -25,6 +25,7 @@
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
 
        // syncfusion bundles
+       '@syncfusion': 'npm:@syncfusion',
        "@syncfusion/ej2-base": "syncfusion:ej2-base/dist/ej2-base.umd.min.js",
        "@syncfusion/ej2-buttons": "syncfusion:ej2-buttons/dist/ej2-buttons.umd.min.js",
        "@syncfusion/ej2-ng-base": "syncfusion:ej2-ng-base/dist/ej2-ng-base.umd.min.js",
